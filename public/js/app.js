104706323477efe9d9435b3accefce868d3634ea
Terra Roush <terra.roush@gmail.com>
2021-09-25 11:23:02 -0500

    created start script, adjusted port for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'loading...'
     messageTwo.textContent = ''
     
-    fetch(`http://localhost:3000/weather?city=${location}`).then((response) => {
+    fetch(`/weather?city=${location}`).then((response) => {
         response.json().then(data => {
             if (data.error) {
                 messageOne.textContent = ''
